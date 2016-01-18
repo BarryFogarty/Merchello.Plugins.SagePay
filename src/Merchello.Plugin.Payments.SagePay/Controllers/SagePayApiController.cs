825d6d8c0585f52c9f2a751deae607a59a63dfa0
Chris Day <chris@daymedia.co.uk>
2016-01-18 20:22:33 +0000

    Fixed anonymous checkout bug
    To replicate the bug. Proceed to checkout as an anonymous customer, use
    SagePay direct, and enter card details on the SagePay pages.
    
    The redirection to receipt will fail and show a null reference error
    message.
    
    This is for the check against invoice.CustomerKey, which isn't required
    to successfully mark the order as paid (and isn't used in the code that
    follows).
    
    Running Merchello 1.13.2 on Umbraco 7.3.4.

--- src/Merchello.Plugin.Payments.SagePay/Controllers/SagePayApiController.cs
+++ src/Merchello.Plugin.Payments.SagePay/Controllers/SagePayApiController.cs
@@ -111,7 +111,7 @@ namespace Merchello.Plugin.Payments.SagePay.Controllers
             var invoice = _merchelloContext.Services.InvoiceService.GetByKey(invoiceKey);
             var payment = _merchelloContext.Services.PaymentService.GetByKey(paymentKey);
 
-            if (invoice == null || payment == null || invoice.CustomerKey == null)
+            if (invoice == null || payment == null)
             {
                 var ex = new NullReferenceException( string.Format( "Invalid argument exception. Arguments: invoiceKey={0}, paymentKey={1}", invoiceKey, paymentKey));
                 LogHelper.Error<SagePayApiController>("Payment not authorized.", ex);
